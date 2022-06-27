ff701d604ff955f8fa29015668667bd3373fe1aa
Alex Cattell <abcattell91@gmail.com>
2022-06-27 17:00:30 +0100

    started GraphQL with Full Stack MERN Project

---
+++ server/sampleData.js
@@ -0,0 +1,79 @@
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'eCommerce Website',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    status: 'In Progress',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Dating App',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    status: 'In Progress',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'SEO Project',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    status: 'In Progress',
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Design Prototype',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    status: 'Done',
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'Auction Website',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    status: 'In Progress',
+  },
+];
+
+// Clients
+
+const clients = [
+  {
+    id: '1',
+    name: 'Tony Stark',
+    email: 'ironman@gmail.com',
+    phone: '010-999-4333'
+  },
+  {
+    id: '2',
+    name: 'Natasha Romanova',
+    email: 'blackwidow@gmail.com',
+    phone: '020-999-4333'
+  },
+  {
+    id: '3',
+    name: 'Thor Odinson',
+    email: 'thor@gmail.com',
+    phone: '030-999-4333'
+  },
+  {
+    id: '4',
+    name: 'Steve Rogers',
+    email: 'captain@gmail.com',
+    phone: '040-999-4333'
+  },
+  {
+    id: '5',
+    name: 'Bruce Banner',
+    email: 'hulk@gmail.com',
+    phone: '050-999-4333'
+  },
+];
+
+module.exports = { projects, clients };
